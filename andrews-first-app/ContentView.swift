26a6f4f01c6a18ce44c6b541bf479efa61909bbd
Andrew Ferruolo <andrew.ferruoo@gmail.com>
2024-08-14 14:57:32 -0400

    Initial Commit

---
+++ andrews-first-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  andrews-first-app
+//
+//  Created by Andrew Ferruolo on 8/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ andrews-first-app/andrews_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  andrews_first_appApp.swift
+//  andrews-first-app
+//
+//  Created by Andrew Ferruolo on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct andrews_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
