cb39025b7981fcf68b697f4b312b420a27c4b9aa
Andrew Ferruolo <andrew.ferruoo@gmail.com>
2024-08-14 16:02:17 -0400

    ready to deploy

--- andrews-first-app/andrews_first_appApp.swift
+++ andrews-first-app/andrews_first_appApp.swift
@@ -1,14 +1,57 @@
-//
-//  andrews_first_appApp.swift
-//  andrews-first-app
-//
-//  Created by Andrew Ferruolo on 8/14/24.
-//
-
 import SwiftUI
 
+
+struct FirstView: View {
+    var body: some View {
+        VStack {
+            Text("First Screen")
+                .font(.largeTitle)
+            
+            NavigationLink(destination: SecondView()) {
+                Text("Go to Second Screen")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .navigationTitle("First Screen")
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        VStack {
+            Text("Second Screen")
+                .font(.largeTitle)
+            
+            NavigationLink(destination: ThirdView()) {
+                Text("Go to Third Screen")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .navigationTitle("Second Screen")
+    }
+}
+
+struct ThirdView: View {
+    var body: some View {
+        VStack {
+            Text("Third Screen")
+                .font(.largeTitle)
+            
+            Text("Welcome to the Third Screen!")
+                .padding()
+        }
+        .navigationTitle("Third Screen")
+    }
+}
+
 @main
-struct andrews_first_appApp: App {
+struct ThreeScreenApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
